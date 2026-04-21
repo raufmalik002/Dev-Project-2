768675cc4fc27a082eca4b4cbc0cbb382a6009bc
raufmalik002 <raufmalik220@hotmail.com>
2026-04-21 15:04:21 +0500

    Update script.sh file

--- terraform-aws/script.sh
+++ terraform-aws/script.sh
@@ -4,3 +4,17 @@ sudo apt install -y docker.io git
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
+
+# Go to home directory
+cd /home/ubuntu
+
+# Clone your repo (CHANGE THIS)
+git clone https://github.com/raufmalik002/Dev-Project-2.git app
+
+cd app
+
+# Build Docker image
+docker build -t flask-app .
+
+# Run container
+docker run -d -p 5000:5000 --name flask-container flask-app
