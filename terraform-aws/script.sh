de171d23b343a7d0941a479cef654cff955fff1c
raufmalik002 <raufmalik220@hotmail.com>
2026-04-22 01:01:04 +0500

    change in .sh file in which remove app directory and set default Dev-Project-2/app directory where image will form

--- terraform-aws/script.sh
+++ terraform-aws/script.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y docker.io git
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ubuntu
+set -ex   # VERY IMPORTANT (shows errors)
 
-# Go to home directory
+apt update -y
+apt install -y docker.io git
+
+systemctl start docker
+systemctl enable docker
+
+# Add ubuntu user to docker group
+usermod -aG docker ubuntu
+
+# Move to home
 cd /home/ubuntu
 
-# Clone your repo (CHANGE THIS)
-git clone https://github.com/raufmalik002/Dev-Project-2.git app
+# Clone repo
+git clone https://github.com/raufmalik002/Dev-Project-2.git
 
-cd app
+# Go to correct folder (IMPORTANT FIX)
+cd Dev-Project-2/app
 
-# Build Docker image
+# Build image
 docker build -t flask-app .
+
+# Remove old container if exists
+docker rm -f flask-container || true
 
 # Run container
 docker run -d -p 5000:5000 --name flask-container flask-app
